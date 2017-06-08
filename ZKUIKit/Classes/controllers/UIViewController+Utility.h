40bce964e020c7d202701ea8c26575cf8e003d5e
wansong <betterofsong@gmail.com>
2017-06-08 14:31:39 +0800

    0.1.5: add UIViewController+Utility

---
+++ ZKUIKit/Classes/controllers/UIViewController+Utility.h
@@ -0,0 +1,15 @@
+//
+//  VideoPlayerController.h
+//  SmartStudy
+//
+//  Created by wansong.mbp.work on 15/12/10.
+//  Copyright © 2015年 Innobuddy Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Utility)
+
++(UIViewController*) currentViewController;
+
+@end
